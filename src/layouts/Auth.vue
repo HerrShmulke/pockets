e8993f2a2a27d94bb06f4ed8e4ab2ea4f517fd1e
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-07-28 01:54:31 +0700

    feat: add auth layout

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="auth-layout">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AuthLayout',
+  };
+</script>
+
+<style lang="scss">
+  .auth-layout {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
