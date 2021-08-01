6f12a758f86a7331955d16d4cacf9454f508d4ca
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-08-02 01:19:48 +0700

    feat: add forgot password page

--- src/modules/auth/constants/router.js
+++ src/modules/auth/constants/router.js
@@ -1,2 +1,3 @@
 export const AUTH = 'Auth';
 export const AUTH_SIGNIN = 'AuthSignin';
+export const FORGOT_PASSWORD = 'ForgotPassword';
--- src/modules/auth/router.js
+++ src/modules/auth/router.js
@@ -1,15 +1,20 @@
-import { AUTH, AUTH_SIGNIN } from './constants/router.js';
+import { AUTH, AUTH_SIGNIN, FORGOT_PASSWORD } from './constants/router.js';
 
 const moduleRoute = {
   path: '/auth',
   component: () => import('@/layouts/Auth.vue'),
   name: AUTH,
-  redirect: { name: 'Auth.Signin' },
+  redirect: { name: AUTH_SIGNIN },
   children: [
     {
       path: 'signin',
       name: AUTH_SIGNIN,
       component: () => import('./views/SignIn.vue'),
+    },
+    {
+      path: 'forgot-password',
+      name: FORGOT_PASSWORD,
+      component: () => import('./views/ForgotPassword.vue'),
     },
   ],
 };
