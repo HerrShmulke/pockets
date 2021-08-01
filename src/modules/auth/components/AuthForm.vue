8e5ab4baca7a55431b3015b9afa693d31bec1d38
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-08-02 01:20:02 +0700

    feat: add auth form

---
+++ src/modules/auth/components/AuthForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="auth-form" v-bind="$attrs">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'SignInForm',
+  };
+</script>
+
+<style lang="scss">
+  .auth-form {
+    display: flex;
+    flex-direction: column;
+
+    & > .v-button {
+      margin-top: 40px;
+    }
+
+    & > .text-field {
+      margin-bottom: 16px;
+    }
+  }
+</style>
